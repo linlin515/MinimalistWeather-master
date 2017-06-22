b4aa4843e376b2ef044bccc70f1cb80b3db3d63e
linlin515 <2219143142@qq.com>
2017-06-22 17:16:20 +0800

    Initial commit

---
+++ widget/src/main/java/com/baronzhang/android/widget/IndicatorValueChangeListener.java
@@ -0,0 +1,11 @@
+package com.baronzhang.android.widget;
+
+/**
+ * Created by baron on 14-8-28.
+ *
+ * @author baronzhang (baron[dot]zhanglei[at]gmail[dot]com)
+ */
+public interface IndicatorValueChangeListener {
+
+    void onChange(int currentIndicatorValue, String stateDescription, int indicatorTextColor);
+}
